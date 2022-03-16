970ee741c03c801782cb5f6d65276f223921c538
r-ILIASS <iliass.root@gmail.com>
2022-03-16 17:02:56 +0100

    refactor[controllers/products.js]: extract query checking logic to models/product.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,73 +1,13 @@
-const Product = require("../models/product");
+const { Product, checkQuery } = require("../models/product");
 
+// For manual tests
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ price: { $gt: 30 } }).sort("price");
   res.status(200).json(products);
 };
 
 const getAllProducts = async (req, res) => {
-  const query = {};
-  let sortStr = "";
-  let selectStr = "";
-  let limit = 10;
-  let page = 1;
-  let skip = 0;
-
-  // Cheking for query parameters
-  if ("featured" in req.query) {
-    query.featured = req.query.featured;
-  }
-  if ("company" in req.query) {
-    query.company = { $regex: req.query.company, $options: "i" };
-  }
-  if ("name" in req.query) {
-    query.name = { $regex: req.query.name, $options: "i" };
-  }
-  if ("sort" in req.query) {
-    sortStr = req.query.sort.replace(",", " ").replace(/\s+/g, " ").trim();
-  }
-  if ("select" in req.query) {
-    selectStr = req.query.select.replace(",", " ").replace(/\s+/g, " ").trim();
-  }
-  if ("limit" in req.query) {
-    limit = Number(req.query.limit);
-  }
-  if ("page" in req.query) {
-    page = Number(req.query.page);
-    // Pagination logic
-    skip = (page - 1) * limit;
-  }
-  if ("numericFilters" in req.query) {
-    const options = ["price", "rating"];
-    const operatorMap = {
-      ">": "$gt",
-      ">=": "$gte",
-      "=": "$eq",
-      "<": "$lt",
-      "<=": "$lte",
-    };
-
-    // Regular expression to match [< > <= >= =]
-    const regEx = /\b(>|>=|=|<|<=)\b/g;
-    // Replace all matches with the values from the map
-    let filters = req.query.numericFilters.replace(
-      regEx,
-      (match) => `-${operatorMap[match]}-`
-    );
-
-    // Create an array of filters
-    filters = filters.split(",");
-
-    // Loop through the array to construct a mongoose
-    // friendly query then add it to the query variable
-    // ex: price-$gte-30 turns into price: {'$gte': 30}
-    filters.forEach((item) => {
-      const [field, operator, value] = item.split("-");
-      if (options.includes(field)) {
-        query[field] = { [operator]: Number(value) };
-      }
-    });
-  }
+  const { query, sortStr, selectStr, limit, skip } = checkQuery(req.query);
 
   // Fetching products
   const products = await Product.find(query)
--- models/product.js
+++ models/product.js
@@ -30,4 +30,78 @@ const schema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Product", schema);
+function checkQuery(queryObj) {
+  const query = {};
+  let sortStr = "";
+  let selectStr = "";
+  let limit = 10;
+  let page = 1;
+  let skip = 0;
+
+  // Cheking for query parameters
+  if ("featured" in queryObj) {
+    query.featured = queryObj.featured;
+  }
+  if ("company" in queryObj) {
+    query.company = { $regex: queryObj.company, $options: "i" };
+  }
+  if ("name" in queryObj) {
+    query.name = { $regex: queryObj.name, $options: "i" };
+  }
+  if ("sort" in queryObj) {
+    sortStr = queryObj.sort.replace(",", " ").replace(/\s+/g, " ").trim();
+  }
+  if ("select" in queryObj) {
+    selectStr = queryObj.select.replace(",", " ").replace(/\s+/g, " ").trim();
+  }
+  if ("limit" in queryObj) {
+    limit = Number(queryObj.limit);
+  }
+  if ("page" in queryObj) {
+    page = Number(queryObj.page);
+    // Pagination logic
+    skip = (page - 1) * limit;
+  }
+  if ("numericFilters" in queryObj) {
+    const options = ["price", "rating"];
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    // Regular expression to match [< > <= >= =]
+    const regEx = /\b(>|>=|=|<|<=)\b/g;
+    // Replace all matches with the values from the map
+    let filters = queryObj.numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    // Create an array of filters
+    filters = filters.split(",");
+
+    // Loop through the array to construct a mongoose
+    // friendly query then add it to the query variable
+    // ex: price-$gte-30 turns into price: {'$gte': 30}
+    filters.forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        query[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  return {
+    query,
+    sortStr,
+    selectStr,
+    limit,
+    skip,
+  };
+}
+
+exports.Product = mongoose.model("Product", schema);
+exports.checkQuery = checkQuery;
